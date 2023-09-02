924f32c69a7419e82a6c784161ab4a8d4f29d918
shivakumar gani <shivakumargani8@gmail.com>
2023-09-02 23:13:44 +0530

    Initial commit

---
+++ FirstMissingPositive.java
@@ -0,0 +1,29 @@
+/*
+Given an unsorted integer array nums, return the smallest missing positive integer.
+*/
+
+import java.util.*;
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums={1,2,0};
+        System.out.println(firstMissingPositive(nums));//O/P: 3 . The smallest missing positive number is 3
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        TreeSet<Integer> set=new TreeSet<>();
+        for(int i: nums){
+            if(i<=0)
+            continue;
+            else set.add(i);
+        }
+        int maxPositive=0; //Concider 0 as the first missing positive integer
+        if(set.size()>0){//If positive numbers exists in tree
+            maxPositive= set.last(); //Stores maximum element
+            for(int i=1;i<=maxPositive;i++)
+                if(set.contains(i))//Checks do we have each positive numbers from 1.
+                    continue;
+                else return i; //If number not exists return current positive number
+        }
+        return (maxPositive+1);//If all elements exists return maxPositive+1(Next element in the arrary)
+    }
+}
